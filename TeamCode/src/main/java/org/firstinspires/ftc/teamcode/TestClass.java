09e0d2c8eb3f28022f78d83ad1c4c4217b28d7fc
bouyio <124091119+bouyio@users.noreply.github.com>
2025-07-05 13:51:20 +0300

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.github.bouyio.cyancore.geomery.Point;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+/**This is a test class for version control and build systems.*/
+public class TestClass extends OpMode {
+    @Override
+    public void init() {
+        Point point = new Point(4, 5);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
